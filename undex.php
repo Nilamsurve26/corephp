2311f6e982841d0eebb255986e8ef4976ef58228
Nilamsurve26 <nilamsurve675@gmail.com>
2022-08-07 21:11:34 +0530

    Datatype in php

--- undex.php
+++ undex.php
@@ -10,15 +10,37 @@
 
     <?php
     $a="apple";
+    $color="red";
 
     echo  "$a";
     ECHO "Hello World!<br>";
 echo "Hello World!<br>";
 EcHo "Hello World!<br>";
-
+echo "my favourite color is .$color.";
     echo "nilam Surve $a";
+    $x=10;
+    $y=5;
+    echo $x;
+    echo "the value of variable is " .$x. "and addtion is" .$y;
+    echo $x + $y;
+   
+$u= 5; // global scope
 
-    ?>
+function myTest() {
+  echo "<p>Variable x inside function is: </p>" .$u;
+}
+myTest();
+
+echo "<p>Variable x outside function is: $u</p>";
+?>
+<?php
+$x = 5985;
+var_dump($x);
+?>
+<?php
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+?>
 
 
 </body>
